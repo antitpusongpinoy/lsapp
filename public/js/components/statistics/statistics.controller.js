a3edaa78f5e6aa63d8e74f689838f8c5d1d46466
Francis Fernandez <franzfernandez163@gmail.com>
2019-04-21 19:19:16 +0800

    statistics

--- public/js/components/statistics/statistics.controller.js
+++ public/js/components/statistics/statistics.controller.js
@@ -15,7 +15,18 @@
             'Municipality':['Municipality','Facility'],
             'Facility':['Facility'],
         }
+        vm.userRole = 'Admin';
         vm.years = [];
+        vm.locationsToGenerate = [];
+
+        vm.allLocations = [];
+        vm.selectedRegions = [];
+        vm.selectedProvinces = [];
+        vm.selectedMuncities = [];
+        vm.selectedFacilities = [];
+
+        vm.recentlySelected = {};
+        vm.periods = ['Year','Quarter','Month'];
         vm.months = [
             {num:1,name:"January"},
             {num:2,name:"February"},
@@ -30,29 +41,70 @@
             {num:11,name:"November"},
             {num:12,name:"December"},
         ];
-        vm.periods = ['Year','Quarter','Month'];
         vm.ranges = {
             Quarter:[{name:'1st',num:1},{name:'2nd',num:2},{name:'3rd',num:3},{name:'4th',num:4}],
             Month:vm.months
         }
+
+        vm.locations = {
+            'Region':[],
+            'Province':[],
+            'Municipality':[],
+            'Facility':[]
+        };
+        vm.filtered = {
+            'Region':[],
+            'Province':[],
+            'Municipality':[],
+            'Facility':[]
+        };
+
+
         vm.programs = [
             {id:"morbidity", name:"Morbidity Diseases"},
             {id:"childcare", name:"Child Care"},
             {id:"mc", name:"Maternal Care"},
             {id:"fp", name:"Family Planning"}
         ];
+
         vm.statsCriterions = {
             "cmpLvl":{
                 'codes':[],
                 'level':''
+            },
+            "year":{
+                'from':0,
+                'to':0,
             }
         };
-        $http.get('api/get/submitted/years')
+        for(var i = 2009; i<=2022; i++)
+            vm.years.push({name:i, num:i});
+            vm.ranges.Year = vm.years;
+            vm.statsCriterions.year.from = vm.years[0].num;
+            vm.statsCriterions.year.to = vm.years[0].num;
+        
+        /* $http.get('api/get/submitted/years')
          .then(function(response){
              console.log(response.data);
              vm.years = response.data;
              vm.statsCriterions.year = vm.years[0];
          }).catch(function(response){
              console.log(response.data);
-         });
+         }); */
+         vm.hasAccessTo = function(level){
+            if(vm.accessLevels[vm.userRole].indexOf(level) != -1)
+                return true;
+            return false;
+        }
+
+        vm.loadAllWAHLocations = function(){
+            $http.get('api/get/wah/regions')
+            .then(function(response){
+                vm.locations.Regions = response.data;
+            })
+            .catch(function(response){
+                alert('Could not retrieve active regions.');
+            })
+        }
+        vm.loadAllWAHLocations();
     }
